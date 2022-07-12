ea2532e8e8ba9505301481aa779b02314f8092ed
Vesnushkka <natasha.kochneva@gmail.com>
2022-07-12 12:09:07 +0500

    Task changed

--- src/__tests__/basic.js
+++ src/__tests__/basic.js
@@ -3,11 +3,13 @@ import { Validator } from "../index.js";
 test("checkFirstUserName", () => {
     let userName = new Validator("Ivan");
     const result = userName.validateUsername(userName);
+    expect(result).toReturn(true);
     expect(result).toReturn(false);
 });
+
 
 test("checkSecondUserName", () => {
     let user2Name = new Validator("Iva_123n");
     const result2 = user2Name.validateUsername(user2Name);
-    expect(result2).toReturn(true);
-});git 
+    expect(result2).toReturn(true, false);
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export class Validator {
   validateUsername(userName) {
-    console.log(/\w-/.test(userName));
-    console.log(/^([^\d\-_])\d{4}([^\d\-_])$/.test(userName));
+    /([a-zA-Z0-9_])*/.test(userName);
+    /^([^\d\-_])\d{4}([^\d\-_])$/.test(userName);
   }
 }
 
